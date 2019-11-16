013a26238e91fe2e85858e72391831f382d19e65
Kent Ahon <kentahon04@gmail.com>
2019-11-16 11:18:48 +0800

    fix recursive symbolic links

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const { readdirSync, readlinkSync, lstatSync, statSync } = require("fs")
 const { resolve, basename } = require("path");
 
+const usage = `\
+usage: narra [<directory list>]
+`
+// XXX: use global?
+const resolvedLinks = new Set();
+
 function recurseDirs(path, config, prefix = "") {
     let ents = readdirSync(path),
         { writer } = config;
@@ -11,15 +17,19 @@ function recurseDirs(path, config, prefix = "") {
         writer.write(`${prefix}${i === len - 1 ? "└── " : "├── "}${basename(fpath)}`)
 
         if (type === "link") {
+            fpath = resolve(path, rpath);
             writer.write(` -> ${rpath}`)
-            fpath = rpath;
+            if (resolvedLinks.has(fpath)) {
+                writer.write(" [recursive, not followed]\n");
+                continue;
+            } else resolvedLinks.add(fpath);
+            
             type = rtype;
         }
 
         writer.write("\n");
-        if (type === "dir") {
-            recurseDirs(fpath, config, `${prefix}${i === len - 1 ? "    " : "│   "}`)
-        }
+        if (type === "dir") recurseDirs(fpath, config, `${prefix}${i === len - 1 ? "    " : "│   "}`)
+        
     }
 }
 
@@ -54,15 +64,33 @@ function main() {
     let { writer } = config;
 
     for (const arg of args) {
-        if (arg[0] === "-" && arg[1]) {
-	    writer.write(`-${arg[1]} not implemented\n`)
-            process.exit(1);
-        } else {
-            paths.push(arg);
-        }
+        if (!restF && arg[0] === "-" && arg[1]) {
+            switch (arg[1]) {
+                case "h":
+                    stderr.write(usage);
+                    exit(0);
+                    break;
+                case "-":
+                    restF = true;
+                    break;
+                default:
+                    stderr.write(`-${arg[1]} not implemented\n`)
+                    stderr.write(usage);
+                    exit(1);
+            }
+        } else paths.push(arg);
+        
     }
-    if (!paths.length) { paths.push("."); }
+    if (!paths.length) paths.push("."); 
     for (const p of paths) {
+        try {
+            accessSync(p);  
+        } catch (e) {
+            if (e.code === "ENOENT") {
+                process.stderr.write(`${p} [error opening dir]\n`)
+                process.exit(2);
+            } else throw e; 
+        }
         writer.write(`${p}\n`);
         recurseDirs(p, config);
     }
