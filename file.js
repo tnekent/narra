7aa3e162242bbc894fee5f56ba7f5975c9c50302
Kent Ahon <kentahon04@gmail.com>
2019-11-26 08:22:30 +0800

    fix: no traversing due to xdev undefined

--- file.js
+++ file.js
@@ -2,11 +2,10 @@ const { readdirSync, readlinkSync, lstatSync, statSync } = require("fs");
 const { IS_FILE, IS_LINK, IS_DIR, getFtype, saveIno, existsIno } = require("./util");
 
 let filecount = 0, dircount = 0;
-let config;
 let inotable = [];
 let xdev;
 
-function filterDirs(ents, path) {
+function filterDirs(ents, path, config) {
     let filtered = [];
     let { all, dirsOnly } = config;
 
@@ -42,11 +41,11 @@ function filterDirs(ents, path) {
     return filtered;
 }
 
-function recurseDirs(path, prefix = "") {
+function recurseDirs(path, config, prefix = "") {
     let ents = readdirSync(path),
         { writer, follow, levels, fullpath } = config;
 
-    ents = filterDirs(ents, path);
+    ents = filterDirs(ents, path, config);
     for (let i = 0, len = ents.length; i < len; i++) {
         let { type, bpath, fpath, lpath, dev, inode } = ents[i];
         writer.write(`\n${prefix}${i === len - 1 ? "└── " : "├── "}${fullpath ? fpath : bpath}`);
@@ -59,7 +58,7 @@ function recurseDirs(path, prefix = "") {
         if (type & IS_FILE) filecount++;
         else if (type & IS_DIR) {
             dircount++;
-            if (dev !== xdev) continue;
+            if (xdev && dev !== xdev) continue;
             if (type & IS_LINK) {
                 if (!follow) continue;
                 else if (existsIno(inotable, dev, inode)) {
@@ -70,16 +69,15 @@ function recurseDirs(path, prefix = "") {
             if (levels !== -1) config.levels = levels - 1;
             if (levels) {
                 saveIno(inotable, dev, inode);
-                recurseDirs(fpath, `${prefix}${i === len - 1 ? "    " : "│   "}`);
+                recurseDirs(fpath, config, `${prefix}${i === len - 1 ? "    " : "│   "}`);
             }
         }
     }
 }
 
-function traverseDownFrom(path, _config) {
-    if (_config.xdev) xdev = statSync(path).dev;
-    config = { ..._config };
-    recurseDirs(path);
+function traverseDownFrom(path, config) {
+    if (config.xdev) xdev = statSync(path).dev;
+    recurseDirs(path, config);
     return [filecount, dircount];   
 }
 
